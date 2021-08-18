37f6d24939468f045c8edb4546613242b638e112
Paul Easter <paul.john.easter@gmail.com>
2021-08-18 16:00:10 +1000

    Modified to use canvas, ready to test

--- www/index.js
+++ www/index.js
@@ -3,7 +3,7 @@ import { Universe, Cell } from "wasm-game-of-life";
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
+const ALIVE_COLOR = "#0000AA";
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new();
